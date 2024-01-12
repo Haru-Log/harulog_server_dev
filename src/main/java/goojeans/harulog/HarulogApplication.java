1c1deeb81adf7fe208eda2055891fb1405dad3c4
Keon <lune0410k@naver.com>
2024-01-13 01:37:54 +0900

    test: test contoller added

--- src/main/java/goojeans/harulog/HarulogApplication.java
+++ src/main/java/goojeans/harulog/HarulogApplication.java
@@ -2,8 +2,9 @@ package goojeans.harulog;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 public class HarulogApplication {
 
 	public static void main(String[] args) {
