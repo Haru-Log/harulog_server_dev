81860f29ba76c92803b14ab6c6efeb91508202a5
HJunng <guswjd1gh@gmail.com>
2024-01-17 03:26:33 +0900

    fix: Test 용이성을 위한 어노테이션 추가. - AllArgsConstructor 추가

--- src/main/java/goojeans/harulog/domain/entity/ChatRoom.java
+++ src/main/java/goojeans/harulog/domain/entity/ChatRoom.java
@@ -1,12 +1,14 @@
 package goojeans.harulog.domain.entity;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Entity
 @Getter
 @NoArgsConstructor
+@AllArgsConstructor
 public class ChatRoom extends BaseEntity{
 
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
