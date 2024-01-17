6cadd046f22ed5bd5ef8a03630bf226f1ed17bbb
Keon <lune0410k@naver.com>
2024-01-17 16:07:44 +0900

    chore: fetch type change Fetch type change into lazy.

--- src/main/java/goojeans/harulog/domain/entity/User.java
+++ src/main/java/goojeans/harulog/domain/entity/User.java
@@ -40,9 +40,9 @@ public class User extends BaseEntity{
     private String contactNumber;
 
     //TODO: 순환 참조 일어나지 않는지 확인 필요
-    @OneToMany(mappedBy = "following")
+    @OneToMany(mappedBy = "following", fetch = FetchType.LAZY)
     private List<Follow> followings;
 
-    @OneToMany(mappedBy = "follower")
+    @OneToMany(mappedBy = "follower", fetch = FetchType.LAZY)
     private List<Follow> followers;
 }
