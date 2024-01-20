b9cf35d9bedf11e5eb38da8453d8adb8bcfba5a2
HJunng <guswjd1gh@gmail.com>
2024-01-20 15:42:50 +0900

    feat: 채팅방 Repository 생성 및 테스트

--- src/main/java/goojeans/harulog/chat/domain/entity/ChatRoom.java
+++ src/main/java/goojeans/harulog/chat/domain/entity/ChatRoom.java
@@ -3,13 +3,16 @@ package goojeans.harulog.chat.domain.entity;
 import goojeans.harulog.domain.entity.BaseEntity;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Entity
+@Builder
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
+@Table(name = "chatroom")
 public class ChatRoom extends BaseEntity {
 
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
