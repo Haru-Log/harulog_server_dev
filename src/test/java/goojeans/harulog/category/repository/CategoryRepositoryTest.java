a07bb3654c2c035691eab00a974ccf9b3c85c72f
je326 <mya326@naver.com>
2024-01-20 12:13:49 +0900

    fix: BaseEntity 컬럼 추가

--- src/test/java/goojeans/harulog/category/repository/CategoryRepositoryTest.java
+++ src/test/java/goojeans/harulog/category/repository/CategoryRepositoryTest.java
@@ -9,6 +9,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 @Slf4j
@@ -18,6 +20,24 @@ class CategoryRepositoryTest {
 
     @Autowired
     CategoryRepository categoryRepository;
+
+    @Test
+    @DisplayName("카테고리 생성")
+    void createCategory() {
+        Category category = Category.builder()
+                .categoryName("test")
+                .build();
+
+        Category savedCategory = categoryRepository.save(category);
+
+        log.info(String.valueOf(savedCategory.getCreatedAt()));
+        log.info(String.valueOf(savedCategory.getUpdatedAt()));
+        log.info(String.valueOf(savedCategory.getActiveStatus()));
+
+        List<Category> categories = categoryRepository.findAll();
+
+        Assertions.assertThat(categories).hasSize(5);
+    }
 
     @Test
     @DisplayName("카테고리 이름으로 조회하기")
