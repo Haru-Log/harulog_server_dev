afdc62c362c24857c7fa0873c85ff33417c95ca3
Keon <lune0410k@naver.com>
2024-01-21 22:03:27 +0900

    refactor: social type erased Social type google and naver erased.

--- src/main/resources/data.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO category (category_name, created_at, updated_at, active_status) VALUES ('운동', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, 'ACTIVE');
-INSERT INTO category (category_name, created_at, updated_at, active_status) VALUES ('기상', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, 'ACTIVE');
-INSERT INTO category (category_name, created_at, updated_at, active_status) VALUES ('독서', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, 'ACTIVE');
-INSERT INTO category (category_name, created_at, updated_at, active_status) VALUES ('공부', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, 'ACTIVE');
