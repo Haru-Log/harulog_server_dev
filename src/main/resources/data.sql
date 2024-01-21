1f33eca4eae96c324648527a181ac431a12fcdf5
Keon <lune0410k@naver.com>
2024-01-21 22:30:10 +0900

    feat: sql init added

---
+++ src/main/resources/data.sql
@@ -0,0 +1,4 @@
+INSERT INTO category (category_name, created_at, updated_at, active_status) VALUES ('운동', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, 'ACTIVE');
+INSERT INTO category (category_name, created_at, updated_at, active_status) VALUES ('기상', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, 'ACTIVE');
+INSERT INTO category (category_name, created_at, updated_at, active_status) VALUES ('독서', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, 'ACTIVE');
+INSERT INTO category (category_name, created_at, updated_at, active_status) VALUES ('공부', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, 'ACTIVE');
