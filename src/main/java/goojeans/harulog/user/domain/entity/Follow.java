062baf597dc02ab88036eec2c20948920b298584
Keon <lune0410k@naver.com>
2024-01-21 22:05:18 +0900

    refactor: builder fixed and soft delete feat Fix builder and soft delete feature added.

--- src/main/java/goojeans/harulog/user/domain/entity/Follow.java
+++ src/main/java/goojeans/harulog/user/domain/entity/Follow.java
@@ -6,12 +6,15 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.SQLRestriction;
 
-@Builder
 @Getter
 @Entity
 @AllArgsConstructor
 @NoArgsConstructor
+@SQLDelete(sql = "UPDATE follow SET active_status = 'DELETED' WHERE (follwer_id = ? AND follwing_id = ?)")
+@SQLRestriction("active_status <> 'DELETED'")
 public class Follow extends BaseEntity {
 
     @EmbeddedId
@@ -20,11 +23,20 @@ public class Follow extends BaseEntity {
     @MapsId("followerId")
     @ManyToOne
     @JoinColumn(name = "follower_id")
-    private Users following;
+    private Users follower;
 
     @MapsId("followingId")
     @ManyToOne
     @JoinColumn(name = "following_id")
-    private Users follower;
+    private Users following;
+
+    @Builder
+    public static Follow create(Users follower, Users following) {
+        Follow follow = new Follow();
+        follow.id = new FollowId(follower.getId(), following.getId());
+        follow.follower = follower;
+        follow.following = following;
+        return follow;
+    }
 
 }
--- src/main/java/goojeans/harulog/user/domain/entity/FollowId.java
+++ src/main/java/goojeans/harulog/user/domain/entity/FollowId.java
@@ -1,10 +1,14 @@
 package goojeans.harulog.user.domain.entity;
 
 import jakarta.persistence.Embeddable;
+import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
 
 import java.io.Serializable;
 
+@AllArgsConstructor
+@NoArgsConstructor
 @Embeddable
 @EqualsAndHashCode
 public class FollowId implements Serializable {
