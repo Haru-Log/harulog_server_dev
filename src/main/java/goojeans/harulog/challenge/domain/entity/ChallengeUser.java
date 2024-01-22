048f125524bd1969336150d287f0cc2c0d831dd9
Keon <lune0410k@naver.com>
2024-01-22 17:10:56 +0900

    git: avoid git aborting

--- src/main/java/goojeans/harulog/challenge/domain/entity/ChallengeUser.java
+++ src/main/java/goojeans/harulog/challenge/domain/entity/ChallengeUser.java
@@ -5,16 +5,16 @@ import goojeans.harulog.user.domain.entity.Users;
 import goojeans.harulog.challenge.util.ChallengeRole;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotNull;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import lombok.*;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.SQLRestriction;
 
 @Entity
-@Builder
 @Getter
-@NoArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor
+@SQLDelete(sql = "UPDATE ChallengeUser SET activeStatus = 'DELETED' WHERE id = ?")
+@SQLRestriction("active_status = 'ACTIVE'")
 @Table(name = "challenge_user")
 public class ChallengeUser extends BaseEntity {
 
@@ -36,14 +36,30 @@ public class ChallengeUser extends BaseEntity {
 
     @Enumerated(EnumType.STRING)
     @NotNull
-    private ChallengeRole role;
+    private ChallengeRole role = ChallengeRole.PARTICIPANT;
 
-    public void addUser(Users user) {
-        if (this.user != user) {
-            this.user = user;
-        }
-        if (!user.getChallengeUsers().contains(this)) {
-            user.addChallengeUser(this);
+    //정적 팩토리 메서드
+    public static ChallengeUser createChallengeUser(Users user, Challenge challenge) {
+        ChallengeUserPK challengeUserPK = new ChallengeUserPK(user.getId(), challenge.getChallengeId());
+
+        ChallengeUser challengeUser = new ChallengeUser();
+        challengeUser.challengeUserPK = challengeUserPK;
+        challengeUser.challenge = challenge;
+        challengeUser.user = user;
+        challengeUser.role = ChallengeRole.PARTICIPANT;
+
+        return challengeUser;
+    }
+
+    public void assignToChallenge(Challenge challenge) {
+        this.challenge = challenge;
+    }
+
+    public void updateRole() {
+        if (this.role == ChallengeRole.LEADER) {
+            this.role = ChallengeRole.PARTICIPANT;
+        } else {
+            this.role = ChallengeRole.LEADER;
         }
     }
 }
--- src/main/java/goojeans/harulog/user/domain/entity/Users.java
+++ src/main/java/goojeans/harulog/user/domain/entity/Users.java
@@ -4,7 +4,6 @@ import com.fasterxml.jackson.annotation.JsonManagedReference;
 import goojeans.harulog.challenge.domain.entity.ChallengeUser;
 import goojeans.harulog.chat.domain.entity.ChatRoomUser;
 import goojeans.harulog.domain.entity.BaseEntity;
-import goojeans.harulog.post.domain.entity.Post;
 import goojeans.harulog.user.util.SocialType;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotNull;
@@ -12,8 +11,6 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import org.hibernate.annotations.SQLDelete;
-import org.hibernate.annotations.SQLRestriction;
 
 import java.util.HashSet;
 import java.util.List;
@@ -33,8 +30,6 @@ import java.util.Set;
                 )
         }
 )
-@SQLDelete(sql = "UPDATE users SET active_status = 'DELETED' WHERE user_id = ?")
-@SQLRestriction("active_status <> 'DELETED'")
 public class Users extends BaseEntity {
 
     @Id @Column(name = "user_id")
@@ -59,34 +54,18 @@ public class Users extends BaseEntity {
 
     private String contactNumber;
 
-    private String imageUrl;
+    @OneToMany(mappedBy = "following", fetch = FetchType.LAZY)
+    private List<Follow> followings;
+
+    @OneToMany(mappedBy = "follower", fetch = FetchType.LAZY)
+    private List<Follow> followers;
 
     @OneToMany(mappedBy = "user", fetch = FetchType.LAZY)
     @Builder.Default
     private Set<ChallengeUser> challengeUsers = new HashSet<>();
 
-    @OneToMany(mappedBy = "user", fetch = FetchType.LAZY)
-    @Builder.Default
-    private Set<Post> posts = new HashSet<>();
-
     // [채팅방 목록]을 위한 양방향 매핑
     @OneToMany(mappedBy = "user", fetch = FetchType.LAZY)
     @JsonManagedReference
     private List<ChatRoomUser> chatRoomUsers;
-
-    // 연관 관계 편의 메서드
-    public void addChallengeUser(ChallengeUser challengeUser) {
-        this.challengeUsers.add(challengeUser);
-        if (challengeUser.getUser() != this) {
-            challengeUser.addUser(this);
-        }
-    }
-
-    //연관 관계 편의 메서드
-    public void addPost(Post post) {
-        this.posts.add(post);
-        if (post.getUser() != this) {
-            post.addUser(this);
-        }
-    }
 }
