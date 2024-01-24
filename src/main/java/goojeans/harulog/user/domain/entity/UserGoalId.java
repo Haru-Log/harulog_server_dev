cc9bb33c6918effd8fe1df620d6250b9924d2cbe
Keon <lune0410k@naver.com>
2024-01-25 00:25:12 +0900

    feat: constructor added

--- src/main/java/goojeans/harulog/user/domain/entity/UserGoalId.java
+++ src/main/java/goojeans/harulog/user/domain/entity/UserGoalId.java
@@ -1,12 +1,16 @@
 package goojeans.harulog.user.domain.entity;
 
 import jakarta.persistence.Embeddable;
+import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
 
 import java.io.Serializable;
 
 @Embeddable
 @EqualsAndHashCode
+@AllArgsConstructor
+@NoArgsConstructor
 public class UserGoalId implements Serializable {
 
     private Long userId;
