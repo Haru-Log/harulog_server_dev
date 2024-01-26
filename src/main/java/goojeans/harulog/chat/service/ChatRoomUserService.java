beda3c519cdc8d2591d2e7d15429fa0b5eb282dc
HJunng <guswjd1gh@gmail.com>
2024-01-27 02:13:47 +0900

    feat: 채팅방-유저 Service 및 인터페이스 구현

--- src/main/java/goojeans/harulog/chat/service/ChatRoomUserService.java
+++ src/main/java/goojeans/harulog/chat/service/ChatRoomUserService.java
@@ -1,22 +1,22 @@
 package goojeans.harulog.chat.service;
 
-import goojeans.harulog.chat.domain.dto.ChatRoomUserDTO;
-import goojeans.harulog.chat.domain.entity.ChatRoom;
-import goojeans.harulog.chat.domain.entity.ChatRoomUser;
+import goojeans.harulog.chat.domain.dto.ChatRoomDTO;
+import goojeans.harulog.chat.domain.dto.ChatUserDTO;
+import goojeans.harulog.domain.dto.Response;
 
 import java.util.List;
 
 public interface ChatRoomUserService {
 
     // 채팅방에 유저 추가
-    public ChatRoomUserDTO addUser(Long roomId, Long userId);
+    public Response<Void> create(Long roomId, Long userId);
 
     // 채팅방에 유저 삭제
-    public void deleteUser(Long roomId, Long userId);
+    public Response<Void> delete(Long roomId, Long userId);
 
     // 채팅방에 참여하고 있는 유저 조회
-    public List<ChatRoomUserDTO> findByChatRoom(Long roomId);
+    public Response<List<ChatUserDTO>> findByChatRoom(Long roomId);
 
     // 유저가 참여하고 있는 채팅방 조회
-    public List<ChatRoomUserDTO> findByUser(Long userId);
+    public Response<List<ChatRoomDTO>> findByUser(Long userId);
 }
