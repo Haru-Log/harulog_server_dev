104386d4cec32a7737b0b7962b441ccc70e00b64
Keon <lune0410k@naver.com>
2024-01-29 18:20:35 +0900

    feat: error message added

--- src/main/java/goojeans/harulog/domain/BusinessException.java
+++ src/main/java/goojeans/harulog/domain/BusinessException.java
@@ -8,6 +8,7 @@ public class BusinessException extends RuntimeException{
     private final ResponseCode errorCode;
 
     public BusinessException(ResponseCode errorCode) {
+        super(errorCode.getMessage());
         this.errorCode = errorCode;
     }
 }
