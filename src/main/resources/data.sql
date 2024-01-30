7c04c946f80fdb07835434f3edf409a8528130df
Keon <lune0410k@naver.com>
2024-01-30 14:55:50 +0900

    feat: test user added

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -2,3 +2,6 @@ INSERT INTO category (category_name, created_at, updated_at, active_status) VALU
 INSERT INTO category (category_name, created_at, updated_at, active_status) VALUES ('기상', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, 'ACTIVE');
 INSERT INTO category (category_name, created_at, updated_at, active_status) VALUES ('독서', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, 'ACTIVE');
 INSERT INTO category (category_name, created_at, updated_at, active_status) VALUES ('공부', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, 'ACTIVE');
+
+INSERT INTO users (created_at, updated_at, user_id, email, nickname, password, user_name, active_status, social_type, user_role)
+ VALUES('2024-01-01', '2024-01-01', 1, 'test', 'test', '{bcrypt}$2a$10$EKWzqU3GOsgCjOYbKZWYhe4s1.Unz8t6ooyQO6WCP3ksdsn.KLMXe', 'test', 'ACTIVE', 'HARU', 'USER');
