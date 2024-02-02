7309d7ef7f69e45eda5154054dc4edff036cfbbe
HJunng <guswjd1gh@gmail.com>
2024-02-02 16:53:36 +0900

    refactor: 메서드 참조(::)로 변경

--- src/main/java/goojeans/harulog/chat/service/ChatRoomUserServiceImpl.java
+++ src/main/java/goojeans/harulog/chat/service/ChatRoomUserServiceImpl.java
@@ -17,10 +17,13 @@ import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+import static goojeans.harulog.chat.util.ChatRoomType.DM;
+import static goojeans.harulog.chat.util.ChatRoomType.GROUP;
+
 @Slf4j
 @Service
 @RequiredArgsConstructor
-public class ChatRoomUserServiceImpl implements ChatRoomUserService{
+public class ChatRoomUserServiceImpl implements ChatRoomUserService {
 
     private final ChatRoomUserRepository chatRoomUserRepository;
     private final ChatRoomRepository chatRoomRepository;
@@ -40,23 +43,27 @@ public class ChatRoomUserServiceImpl implements ChatRoomUserService{
     @Override
     public Response<Void> addUser(String roomId, List<String> usersNickname) {
 
-            ChatRoom chatRoom = findChatRoom(roomId);
-            List<Users> users = usersNickname.stream()
-                    .map(userNickname -> findUser(userNickname))
-                    .toList();
+        ChatRoom chatRoom = findChatRoom(roomId);
+        List<Users> users = usersNickname.stream()
+                .map(this::findUser)
+                .toList();
 
-            users.stream()
-                    .forEach(user -> chatRoomUserRepository.save(ChatRoomUser.create(chatRoom, user)));
+        users.forEach(user -> chatRoomUserRepository.save(ChatRoomUser.create(chatRoom, user)));
 
-            return Response.ok();
+
+        // DM 채팅방이면서 유저가 2명 초과이면 그룹 채팅방으로 변경
+        if (chatRoom.getType() == DM && !users.isEmpty()) {
+            chatRoom.setType(GROUP);
+            chatRoomRepository.save(chatRoom);
+        }
+
+        return Response.ok();
     }
 
     @Override
     public Response<Void> deleteUser(String roomId, String userNickname) {
 
-        Users user = findUser(userNickname);
-        ChatRoomUser chatRoomUser = chatRoomUserRepository.findByChatRoomIdAndUserId(roomId, user.getId())
-                .orElseThrow(() -> new BusinessException(ResponseCode.CHAT_NO_PERMISSION));
+        ChatRoomUser chatRoomUser = findChatRoomUser(roomId, userNickname);
         chatRoomUserRepository.delete(chatRoomUser);
 
         return Response.ok();
@@ -64,34 +71,38 @@ public class ChatRoomUserServiceImpl implements ChatRoomUserService{
 
     @Override
     public Response<List<ChatUserDTO>> getUsers(String roomId) {
-
         List<Users> userList = chatRoomUserRepository.findUserByChatroomId(roomId);
-        List<ChatUserDTO> userDTOList = userList.stream()
-                .map(user -> ChatUserDTO.of(user))
-                .toList();
 
-        return Response.ok(userDTOList);
+        return Response.ok(userList.stream()
+                .map(ChatUserDTO::of)
+                .toList());
     }
 
     @Override
     public Response<List<ChatRoomDTO>> getChatRooms(String userNickname) {
 
-        Users user = findUser(userNickname);
         List<ChatRoom> chatRoomList = chatRoomUserRepository.findChatRoomsByUserNickName(userNickname);
         List<ChatRoomDTO> chatRoomDTOList = chatRoomList.stream()
-                .map(chatRoom -> ChatRoomDTO.of(chatRoom))
+                .map(ChatRoomDTO::of)
                 .toList();
 
         return Response.ok(chatRoomDTOList);
     }
 
-    private Users findUser(String userNickname){
+    private Users findUser(String userNickname) {
         return userRepository.findUsersByNickname(userNickname)
                 .orElseThrow(() -> new BusinessException(ResponseCode.USER_NOT_FOUND));
     }
 
-    private ChatRoom findChatRoom(String roomId){
+    private ChatRoom findChatRoom(String roomId) {
         return chatRoomRepository.findById(roomId)
                 .orElseThrow(() -> new BusinessException(ResponseCode.CHATROOM_NOT_FOUND));
+    }
+
+    private ChatRoomUser findChatRoomUser(String roomId, String userNickname) {
+        Users user = findUser(userNickname);
+        findChatRoom(roomId);
+        return chatRoomUserRepository.findByChatRoomIdAndUserId(roomId, user.getId())
+                .orElseThrow(() -> new BusinessException(ResponseCode.CHAT_NO_PERMISSION));
     }
 }
