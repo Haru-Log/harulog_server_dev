0ee522b1e37af52ed1fbfb99862719d6aea5e1e3
HJunng <guswjd1gh@gmail.com>
2024-02-05 12:08:13 +0900

    refactor: 인터페이스 멤버는 public 사용 x

--- src/main/java/goojeans/harulog/chat/service/MessageService.java
+++ src/main/java/goojeans/harulog/chat/service/MessageService.java
@@ -7,23 +7,23 @@ import goojeans.harulog.domain.dto.Response;
 public interface MessageService {
 
     // 채팅방 메세지 조회
-    public Response<MessageListDTO> getMessages(String roomId, String userNickname);
+    Response<MessageListDTO> getMessages(String roomId, String userNickname);
 
     // 채팅방 구독 여부 확인
-    public boolean existSubscribe(String roomId, String userNickname);
+    boolean existSubscribe(String roomId, String userNickname);
 
     // 채팅방 입장 여부 확인
-    public boolean isEntered(String roomId, String userNickname);
+    boolean isEntered(String roomId, String userNickname);
 
     // 채팅방 구독
-    public void subscribe(String roomId, String userNickname);
+    void subscribe(String roomId, String userNickname);
 
     // 채팅방 입장
-    public MessageDTO enter(String roomId, String userNickname);
+    MessageDTO enter(String roomId, String userNickname);
 
     // 메세지 전송
-    public MessageDTO send(String roomId, String userNickname, String content);
+    MessageDTO send(String roomId, String userNickname, String content);
 
     // 클라이언트 채팅방 구독 취소
-    public MessageDTO exit(String roomId, String userNickname);
+    MessageDTO exit(String roomId, String userNickname);
 }
