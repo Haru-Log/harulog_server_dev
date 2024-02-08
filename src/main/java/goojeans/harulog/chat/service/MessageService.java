8027097952db467ccd3d1912b5c89594986f5150
HJunng <guswjd1gh@gmail.com>
2024-02-09 05:05:43 +0900

    feat: 입장 로직 변경 - 원래 (입장 = 구독)이 같은 개념으로 갔었는데 RabbitMQ를 사용하면서 다른 개념으로 분리. - 필요 없는 메서드 삭제 - 이름을 보다 더 직관적으로 변경

--- src/main/java/goojeans/harulog/chat/service/MessageService.java
+++ src/main/java/goojeans/harulog/chat/service/MessageService.java
@@ -6,24 +6,15 @@ import goojeans.harulog.domain.dto.Response;
 
 public interface MessageService {
 
-    // 채팅방 메세지 조회
+    // 채팅방 입장 및 메세지 조회
     Response<MessageListDTO> getMessages(String roomId, String userNickname);
 
-    // 채팅방 구독 여부 확인
-    boolean existSubscribe(String roomId, String userNickname);
-
-    // 채팅방 입장 여부 확인
-    boolean isEntered(String roomId, String userNickname);
-
-    // 채팅방 구독
-    void subscribe(String roomId, String userNickname);
-
-    // 채팅방 입장
-    MessageDTO enter(String roomId, String userNickname);
+    // 채팅방 입장 메세지
+    MessageDTO entry(String roomId, String userNickname);
 
     // 메세지 전송
     MessageDTO send(String roomId, String userNickname, String content);
 
-    // 클라이언트 채팅방 구독 취소
+    // 채팅방 퇴장 메세지
     MessageDTO exit(String roomId, String userNickname);
 }
