e7990d25cf897356d379501820158632779ebb46
HJunng <guswjd1gh@gmail.com>
2024-02-10 01:41:12 +0900

    feat: 채팅방 마다 RabbitMQ의 Exchange 할당 - 채팅방 생성, 삭제 시 Exchange도 생성, 삭제 됨. - 채팅방 조회 테스트 -> Exception 처리가 잘 되었는지 검증 추가.

--- src/test/java/goojeans/harulog/chat/service/ChatRoomServiceTest.java
+++ src/test/java/goojeans/harulog/chat/service/ChatRoomServiceTest.java
@@ -5,6 +5,7 @@ import goojeans.harulog.chat.domain.entity.ChatRoom;
 import goojeans.harulog.chat.repository.ChatRoomRepository;
 import goojeans.harulog.config.RabbitMQConfig;
 import goojeans.harulog.domain.BusinessException;
+import goojeans.harulog.domain.ResponseCode;
 import goojeans.harulog.domain.dto.Response;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
@@ -41,7 +42,6 @@ class ChatRoomServiceTest {
         // given
         ChatRoom chatRoom = new ChatRoom();
         doReturn(chatRoom).when(chatRoomRepository).save(any(ChatRoom.class));
-//        when(chatRoomRepository.save(any(ChatRoom.class))).thenReturn(chatRoom);
 
         // when
         Response<ChatRoomDTO> created = chatRoomService.createChatRoom();
@@ -79,8 +79,11 @@ class ChatRoomServiceTest {
         // when
 
         // then
+        // 채팅방이 존재하지 않을 때의 에러 메세지 확인
         Assertions.assertThatThrownBy(() -> chatRoomService.findByRoomId(roomId))
-                .isInstanceOf(BusinessException.class);
+                .isInstanceOf(BusinessException.class)                               // BusinessException이 발생하는지 확인
+                .hasMessageContaining(ResponseCode.CHATROOM_NOT_FOUND.getMessage()); // BusinessException의 메세지가 "채팅방을 찾을 수 없습니다."인지 확인
+
         verify(chatRoomRepository).findById(roomId);
     }
 
