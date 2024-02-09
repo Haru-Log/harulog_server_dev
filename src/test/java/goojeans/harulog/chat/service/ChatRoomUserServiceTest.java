fbc8b26a3f324875fdfe0c92c7888f7cd9f3bfb2
HJunng <guswjd1gh@gmail.com>
2024-02-10 03:40:36 +0900

    feat: 채팅방에 유저 추가, 삭제 될 때 -> 입장, 퇴장 메세지 전송 - 입장, 퇴장 메세지 생성 및 저장 - 메세지 전송 - 메세지 전송 부분 테스트 코드 추가

--- src/test/java/goojeans/harulog/chat/service/ChatRoomUserServiceTest.java
+++ src/test/java/goojeans/harulog/chat/service/ChatRoomUserServiceTest.java
@@ -2,10 +2,14 @@ package goojeans.harulog.chat.service;
 
 import goojeans.harulog.chat.domain.dto.ChatRoomDTO;
 import goojeans.harulog.chat.domain.dto.ChatUserDTO;
+import goojeans.harulog.chat.domain.dto.MessageDTO;
 import goojeans.harulog.chat.domain.entity.ChatRoom;
 import goojeans.harulog.chat.domain.entity.ChatRoomUser;
+import goojeans.harulog.chat.domain.entity.Message;
 import goojeans.harulog.chat.repository.ChatRoomRepository;
 import goojeans.harulog.chat.repository.ChatRoomUserRepository;
+import goojeans.harulog.chat.repository.MessageRepository;
+import goojeans.harulog.chat.util.MessageType;
 import goojeans.harulog.domain.BusinessException;
 import goojeans.harulog.domain.ResponseCode;
 import goojeans.harulog.domain.dto.Response;
@@ -15,9 +19,12 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
 
 import java.util.List;
 import java.util.Optional;
@@ -31,16 +38,18 @@ class ChatRoomUserServiceTest {
     @InjectMocks
     private ChatRoomUserServiceImpl chatRoomUserService;
 
-    @Mock
-    private ChatRoomUserRepository chatRoomUserRepository;
-    @Mock
-    private ChatRoomRepository chatRoomRepository;
-    @Mock
-    private UserRepository userRepository;
+    @Mock private ChatRoomUserRepository chatRoomUserRepository;
+    @Mock private ChatRoomRepository chatRoomRepository;
+    @Mock private UserRepository userRepository;
+    @Mock private MessageRepository messageRepository;
+    @Mock private RabbitTemplate rabbitTemplate;
+
+    @Captor
+    private ArgumentCaptor<Message> messageCaptor; // 저장되는 Message 캡처를 위한 ArgumentCaptor
 
     @Test
     @DisplayName("채팅방에 유저 추가")
-    void create() {
+    void addUser() {
 
         // given
         ChatRoom chatRoom = new ChatRoom();
@@ -60,8 +69,31 @@ class ChatRoomUserServiceTest {
     }
 
     @Test
+    @DisplayName("채팅방에 유저 여러멍 추가")
+    void addUsers() {
+
+        // given
+        ChatRoom chatRoom = new ChatRoom();
+        Users user1 = Users.builder().nickname("user1").build();
+        Users user2 = Users.builder().nickname("user2").build();
+        when(chatRoomRepository.findById(chatRoom.getId())).thenReturn(Optional.of(chatRoom));
+        when(userRepository.findUsersByNickname(user1.getNickname())).thenReturn(Optional.of(user1));
+        when(userRepository.findUsersByNickname(user2.getNickname())).thenReturn(Optional.of(user2));
+
+        // when
+        Response<Void> response = chatRoomUserService.addUser(chatRoom.getId(), List.of(user1.getNickname(), user2.getNickname()));
+
+        // then
+        Assertions.assertThat(response).isNotNull();
+        Assertions.assertThat(response.getStatus()).isEqualTo(ResponseCode.SUCCESS.getStatus());
+
+        // chatRoomUserRepository.save 메서드가 몇번 호출되었는지 검증
+        verify(chatRoomUserRepository, times(2)).save(any(ChatRoomUser.class));
+    }
+
+    @Test
     @DisplayName("채팅방에 참여 중인 유저 삭제")
-    void deleteSuccess() {
+    void deleteUserSuccess() {
 
         // given
         String roomId = UUID.randomUUID().toString();
@@ -92,7 +124,7 @@ class ChatRoomUserServiceTest {
 
     @Test
     @DisplayName("존재하지 않는 채팅방-유저 삭제 시 예외 발생")
-    void deleteFail() {
+    void deleteUserFail() {
 
         // given
         String roomId = UUID.randomUUID().toString();
@@ -119,7 +151,7 @@ class ChatRoomUserServiceTest {
 
     @Test
     @DisplayName("채팅방 ID로 참여하고 있는 유저 조회")
-    void findByChatRoomSuccess() {
+    void getUsers() {
 
         // given
         String roomId = UUID.randomUUID().toString();
@@ -138,7 +170,7 @@ class ChatRoomUserServiceTest {
 
     @Test
     @DisplayName("유저 ID로 참여하고 있는 채팅방 조회")
-    void findByUser() {
+    void getChatRooms() {
 
         // given
         Users user = Users.builder()
@@ -156,5 +188,45 @@ class ChatRoomUserServiceTest {
         Assertions.assertThat(response.getData()).hasSize(2);
         verify(chatRoomUserRepository).findChatRoomsByUserNickName(user.getNickname());
 
+    }
+
+    @Test
+    @DisplayName("채팅방 입장 메세지 생성, 저장 및 전송")
+    void sendEnterMessage() {
+
+        // given
+        ChatRoom chatRoom = new ChatRoom();
+        Users user = new Users();
+        ChatRoomUser chatRoomUser = ChatRoomUser.create(chatRoom, user);
+
+        // when
+        chatRoomUserService.sendEnterMessage(chatRoom, user);
+
+        // then
+        verify(messageRepository).save(messageCaptor.capture()); // Message 객체 캡처
+        Message captoredMessage = messageCaptor.getValue();
+        Assertions.assertThat(captoredMessage.getType()).isEqualTo(MessageType.ENTER); // 입장 메세지가 만들어진 것인지 확인.
+
+        verify(rabbitTemplate).convertAndSend(eq("chatroom."+chatRoom.getId()), eq(""), any(MessageDTO.class));
+    }
+
+    @Test
+    @DisplayName("채팅방 퇴장 메세지 생성, 저장 및 전송")
+    void sendExitMessage() {
+
+        // given
+        ChatRoom chatRoom = new ChatRoom();
+        Users user = new Users();
+        ChatRoomUser chatRoomUser = ChatRoomUser.create(chatRoom, user);
+
+        // when
+        chatRoomUserService.sendExitMessage(chatRoom, user);
+
+        // then
+        verify(messageRepository).save(messageCaptor.capture()); // Message 객체 캡처
+        Message captoredMessage = messageCaptor.getValue();
+        Assertions.assertThat(captoredMessage.getType()).isEqualTo(MessageType.EXIT); // 퇴장 메세지가 만들어진 것인지 확인.
+
+        verify(rabbitTemplate).convertAndSend(eq("chatroom."+chatRoom.getId()), eq(""), any(MessageDTO.class));
     }
 }
