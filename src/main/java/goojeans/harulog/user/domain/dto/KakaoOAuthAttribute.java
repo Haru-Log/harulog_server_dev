187e74849238ac121174be0c3256ecab50e9e3c9
Keon <lune0410k@naver.com>
2024-02-11 00:45:51 +0900

    refactor: password encoder use bean

--- src/main/java/goojeans/harulog/user/domain/dto/KakaoOAuthAttribute.java
+++ src/main/java/goojeans/harulog/user/domain/dto/KakaoOAuthAttribute.java
@@ -5,7 +5,7 @@ import goojeans.harulog.user.util.SocialType;
 import goojeans.harulog.user.util.UserRole;
 import lombok.Builder;
 import lombok.Getter;
-import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 import java.util.Map;
 import java.util.UUID;
@@ -15,6 +15,7 @@ public class KakaoOAuthAttribute {
 
     private String nameAttributeKey; // OAuth2 로그인 진행 시 키가 되는 필드 값, PK와 같은 의미
     private OAuth2UserInfo oauth2UserInfo;
+    private PasswordEncoder passwordEncoder;
 
     @Builder
     private KakaoOAuthAttribute(String nameAttributeKey, OAuth2UserInfo oauth2UserInfo) {
@@ -37,7 +38,7 @@ public class KakaoOAuthAttribute {
         return Users.builder()
                 .socialType(SocialType.KAKAO)
                 .email(kakaoOAuthUserInfo.getEmail())
-                .password(PasswordEncoderFactories.createDelegatingPasswordEncoder().encode(UUID.randomUUID().toString()))
+                .password(passwordEncoder.encode(UUID.randomUUID().toString()))
                 .userName("kakao_" + UUID.randomUUID().toString().substring(1, 9))
                 .nickname(kakaoOAuthUserInfo.getNickname() + UUID.randomUUID().toString().substring(1, 9))
                 .imageUrl(kakaoOAuthUserInfo.getImageUrl())
