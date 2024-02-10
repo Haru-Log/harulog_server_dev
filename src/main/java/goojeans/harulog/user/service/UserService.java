2552f190cffafbfe83d50230dc719a95a5fe77b9
Keon <lune0410k@naver.com>
2024-02-10 21:41:22 +0900

    feat: user info edit service added

--- src/main/java/goojeans/harulog/user/service/UserService.java
+++ src/main/java/goojeans/harulog/user/service/UserService.java
@@ -3,9 +3,20 @@ package goojeans.harulog.user.service;
 
 import goojeans.harulog.domain.dto.Response;
 import goojeans.harulog.user.domain.dto.request.SignUpRequest;
+import goojeans.harulog.user.domain.dto.request.UpdatePasswordRequest;
+import goojeans.harulog.user.domain.dto.request.UpdateUserInfoRequest;
+import goojeans.harulog.user.domain.dto.response.UserInfoEditResponse;
 
 public interface UserService {
 
     public Response<Void> signUp(SignUpRequest request);
+
+    public Response<UserInfoEditResponse> getUserInfoForEdit();
+
+    public Response<Void> updateUserInfo(UpdateUserInfoRequest request);
+
+    public Response<Void> updatePassword(UpdatePasswordRequest request);
+
+    public Response<Void> delete(String confirmString);
 
 }
--- src/main/java/goojeans/harulog/user/service/UserServiceImpl.java
+++ src/main/java/goojeans/harulog/user/service/UserServiceImpl.java
@@ -3,12 +3,20 @@ package goojeans.harulog.user.service;
 import goojeans.harulog.domain.BusinessException;
 import goojeans.harulog.domain.ResponseCode;
 import goojeans.harulog.domain.dto.Response;
+import goojeans.harulog.user.domain.dto.JwtUserDetail;
 import goojeans.harulog.user.domain.dto.request.SignUpRequest;
+import goojeans.harulog.user.domain.dto.request.UpdatePasswordRequest;
+import goojeans.harulog.user.domain.dto.request.UpdateUserInfoRequest;
+import goojeans.harulog.user.domain.dto.response.UserInfoEditResponse;
 import goojeans.harulog.user.domain.entity.Users;
 import goojeans.harulog.user.repository.UserRepository;
+import goojeans.harulog.user.util.SecurityUtils;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 @Service
 @Transactional
@@ -16,6 +24,7 @@ import org.springframework.stereotype.Service;
 public class UserServiceImpl implements UserService {
 
     private final UserRepository userRepository;
+    private final SecurityUtils securityUtils;
 
     @Override
     public Response<Void> signUp(SignUpRequest request) {
@@ -34,6 +43,78 @@ public class UserServiceImpl implements UserService {
         Users entity = request.toEntity();
 
         userRepository.save(entity);
+
+        return Response.ok();
+    }
+
+    @Override
+    public Response<UserInfoEditResponse> getUserInfoForEdit() {
+        JwtUserDetail currentUserInfo = securityUtils.getCurrentUserInfo();
+
+        LocalDateTime createdAtLocalTime = currentUserInfo.getCreatedAt();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+
+        String createdAtFormat = createdAtLocalTime.format(formatter);
+
+        UserInfoEditResponse response = UserInfoEditResponse.builder()
+                .email(currentUserInfo.getEmail())
+                .userName(currentUserInfo.getUsername())
+                .createdAt(createdAtFormat)
+                .introduction(currentUserInfo.getIntroduction())
+                .nickname(currentUserInfo.getNickname())
+                .contactNumber(currentUserInfo.getContactNumber())
+                .imageUrl(currentUserInfo.getImageUrl())
+                .build();
+
+        return Response.ok(response);
+    }
+
+    @Override
+    public Response<Void> updateUserInfo(UpdateUserInfoRequest request) {
+        JwtUserDetail currentUserInfo = securityUtils.getCurrentUserInfo();
+
+        Users users = userRepository.findUsersById(currentUserInfo.getId()).stream()
+                .findAny()
+                .orElseThrow(() -> new BusinessException(ResponseCode.USER_NOT_FOUND));
+
+        users.updateNickname(request.getNickname());
+        users.updateIntroduction(request.getIntroduction());
+        users.updateContactNumber(request.getIntroduction());
+
+        return Response.ok();
+    }
+
+    @Override
+    public Response<Void> updatePassword(UpdatePasswordRequest request) {
+        JwtUserDetail currentUserInfo = securityUtils.getCurrentUserInfo();
+
+        if (!request.getBeforePassword().equals(currentUserInfo.getPassword())) {
+            throw new BusinessException(ResponseCode.USER_PASSWORD_NOT_MATCH);
+        }
+
+        Users user = userRepository.findUsersById(currentUserInfo.getId()).stream()
+                .findAny()
+                .orElseThrow(() -> new BusinessException(ResponseCode.USER_NOT_FOUND));
+
+        user.updatePassword(request.getAfterPassword());
+
+        return Response.ok();
+    }
+
+    // confirm string is email
+    @Override
+    public Response<Void> delete(String confirmString) {
+        JwtUserDetail currentUserInfo = securityUtils.getCurrentUserInfo();
+
+        if (!confirmString.equals(currentUserInfo.getEmail())) {
+            throw new BusinessException(ResponseCode.USER_DELETE_STRING_NOT_MATCH);
+        }
+
+        Users user = userRepository.findUsersById(currentUserInfo.getId()).stream()
+                .findAny()
+                .orElseThrow(() -> new BusinessException(ResponseCode.USER_NOT_FOUND));
+
+        userRepository.delete(user);
 
         return Response.ok();
     }
