5cc2b78d9d8cbe705692753b0b7285fb54174cf1
Keon <lune0410k@naver.com>
2024-02-10 21:41:44 +0900

    test: user info edit service test added

--- src/test/java/goojeans/harulog/user/service/UserServiceTest.java
+++ src/test/java/goojeans/harulog/user/service/UserServiceTest.java
@@ -2,25 +2,31 @@ package goojeans.harulog.user.service;
 
 import goojeans.harulog.domain.ResponseCode;
 import goojeans.harulog.domain.dto.Response;
+import goojeans.harulog.user.domain.dto.JwtUserDetail;
 import goojeans.harulog.user.domain.dto.request.SignUpRequest;
+import goojeans.harulog.user.domain.dto.request.UpdatePasswordRequest;
+import goojeans.harulog.user.domain.dto.request.UpdateUserInfoRequest;
+import goojeans.harulog.user.domain.dto.response.UserInfoEditResponse;
 import goojeans.harulog.user.domain.entity.Users;
 import goojeans.harulog.user.repository.UserRepository;
+import goojeans.harulog.user.util.SecurityUtils;
 import goojeans.harulog.user.util.SocialType;
 import goojeans.harulog.user.util.UserRole;
 import lombok.extern.slf4j.Slf4j;
-import org.assertj.core.api.Assertions;
-import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.mockito.Spy;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 
+import java.time.LocalDateTime;
+import java.util.Collection;
+import java.util.Collections;
 import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.*;
@@ -37,7 +43,41 @@ public class UserServiceTest {
     @Mock
     UserRepository userRepository;
 
+    @Mock
+    SecurityUtils securityUtils;
+
     private String testString = "test";
+    private Long testId = 1L;
+
+    private JwtUserDetail jwtUserDetail;
+    private Users testUser;
+
+    @BeforeEach
+    void beforeEach() {
+        testUser = Users.builder()
+                .id(testId)
+                .email(testString)
+                .userName(testString)
+                .nickname(testString)
+                .password(testString)
+                .userRole(UserRole.USER)
+                .socialType(SocialType.HARU)
+                .build();
+        jwtUserDetail = JwtUserDetail.userDetailBuilder()
+                .email(testString)
+                .id(testId)
+                .password(testString)
+                .username(testString)
+                .role(UserRole.USER)
+                .contactNumber(testString)
+                .introduction(testString)
+                .createdAt(LocalDateTime.now())
+                .imageUrl(testString)
+                .nickname(testString)
+                .socialType(SocialType.HARU)
+                .authorities(Collections.singleton(new SimpleGrantedAuthority("USER")))
+                .build();
+    }
 
     @Test
     @DisplayName("회원 가입")
@@ -49,15 +89,6 @@ public class UserServiceTest {
                 .nickname(testString)
                 .password(testString)
                 .userName(testString)
-                .build();
-
-        Users testUser = Users.builder()
-                .email(testString)
-                .userName(testString)
-                .nickname(testString)
-                .password(testString)
-                .userRole(UserRole.USER)
-                .socialType(SocialType.HARU)
                 .build();
 
         doReturn(Optional.empty()).when(userRepository).findUsersByEmail(testString);
@@ -72,6 +103,96 @@ public class UserServiceTest {
         verify(userRepository, times(1)).findUsersByEmail(testString);
         verify(userRepository, times(1)).findUsersByNickname(testString);
         verify(userRepository, times(1)).save(any(Users.class));
+
+    }
+
+    @Test
+    @DisplayName("정보 수정 화면에서의 유저 정보 접근")
+    void getUserInfoForEdit() {
+        //Given
+        doReturn(jwtUserDetail).when(securityUtils).getCurrentUserInfo();
+
+        //When
+        Response<UserInfoEditResponse> response = userService.getUserInfoForEdit();
+
+
+        //Then
+        assertThat(response.getData().getEmail()).isEqualTo(jwtUserDetail.getEmail());
+        verify(securityUtils, times(1)).getCurrentUserInfo();
+
+    }
+
+    @Test
+    @DisplayName("유저 정보 업데이트")
+    void updateUserInfo() {
+        //Given
+        String updateString = "update";
+
+        doReturn(jwtUserDetail).when(securityUtils).getCurrentUserInfo();
+        doReturn(Optional.of(testUser)).when(userRepository).findUsersById(testId);
+
+        UpdateUserInfoRequest request = UpdateUserInfoRequest.builder()
+                .nickname(updateString)
+                .introduction(updateString)
+                .contactNumber(updateString)
+                .build();
+
+        //When
+        Response<Void> response = userService.updateUserInfo(request);
+
+        //Then
+        assertThat(response.getCode()).isEqualTo(ResponseCode.SUCCESS.getCode());
+        assertThat(testUser.getNickname()).isEqualTo(updateString);
+        assertThat(testUser.getIntroduction()).isEqualTo(updateString);
+        assertThat(testUser.getContactNumber()).isEqualTo(updateString);
+
+        verify(userRepository, times(1)).findUsersById(testId);
+
+    }
+
+    @Test
+    @DisplayName("유저 비밀번호 업데이트")
+    void updatePassword() {
+        //Given
+        String updateString = "update";
+
+        UpdatePasswordRequest request = UpdatePasswordRequest.builder()
+                .beforePassword(testString)
+                .afterPassword(updateString)
+                .build();
+
+        doReturn(jwtUserDetail).when(securityUtils).getCurrentUserInfo();
+        doReturn(Optional.of(testUser)).when(userRepository).findUsersById(testId);
+
+        //When
+        Response<Void> response = userService.updatePassword(request);
+
+        //Then
+        assertThat(response.getCode()).isEqualTo(ResponseCode.SUCCESS.getCode());
+        assertThat(testUser.getPassword()).isEqualTo(updateString);
+
+        verify(securityUtils, times(1)).getCurrentUserInfo();
+        verify(userRepository, times(1)).findUsersById(testId);
+
+    }
+
+    @Test
+    @DisplayName("회원 탈퇴")
+    void delete() {
+        //Given
+        doReturn(jwtUserDetail).when(securityUtils).getCurrentUserInfo();
+        doReturn(Optional.of(testUser)).when(userRepository).findUsersById(testId);
+        doNothing().when(userRepository).delete(testUser);
+
+        //When
+        Response<Void> response = userService.delete(testString);
+
+        //Then
+        verify(securityUtils, times(1)).getCurrentUserInfo();
+        verify(userRepository, times(1)).findUsersById(testId);
+        verify(userRepository, times(1)).delete(testUser);
+
+        assertThat(response.getCode()).isEqualTo(ResponseCode.SUCCESS.getCode());
 
     }
 
