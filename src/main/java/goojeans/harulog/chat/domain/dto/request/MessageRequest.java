25bb41c7282a4e2bcff5498d543caa0bad89c9ba
HJunng <guswjd1gh@gmail.com>
2024-02-12 02:30:09 +0900

    feat: 메세지 전송 Controller할 때 Request, Response DTO

--- src/main/java/goojeans/harulog/chat/domain/dto/request/MessageRequest.java
+++ src/main/java/goojeans/harulog/chat/domain/dto/request/MessageRequest.java
@@ -1,14 +1,20 @@
 package goojeans.harulog.chat.domain.dto.request;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 /**
  * 메세지 전송 요청 DTO
  */
 @Getter
+@NoArgsConstructor
 @AllArgsConstructor
 public class MessageRequest {
+    @JsonProperty("sender")
     private String sender; // 유저 닉네임
+
+    @JsonProperty("content")
     private String content; // 메세지 내용
 }
