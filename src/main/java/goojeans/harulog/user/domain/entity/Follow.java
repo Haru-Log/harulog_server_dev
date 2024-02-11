d6768515930ff4ea01ae9785c9dc375e7b613c41
Keon <lune0410k@naver.com>
2024-02-11 15:32:35 +0900

    feat: follow method added

--- src/main/java/goojeans/harulog/user/domain/entity/Follow.java
+++ src/main/java/goojeans/harulog/user/domain/entity/Follow.java
@@ -26,6 +26,24 @@ public class Follow extends BaseEntity {
     @JoinColumn(name = "following_id")
     private Users following;
 
+    public void addFollowing(Users user) {
+        if (this.following != user) {
+            this.following = user;
+        }
+        if (!user.getFollowers().contains(this)) {
+            user.addFollower(this);
+        }
+    }
+
+    public void addFollower(Users user) {
+        if (this.follower != user) {
+            this.follower = user;
+        }
+        if (!user.getFollowings().contains(this)) {
+            user.addFollowing(this);
+        }
+    }
+
     @Builder
     public static Follow of(Users follower, Users following) {
         Follow follow = new Follow();
