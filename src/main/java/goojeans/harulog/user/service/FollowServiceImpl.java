7845a3dae18ed8d11eb0f9a0c0b266ee9bd30ee7
Keon <lune0410k@naver.com>
2024-02-12 02:51:24 +0900

    feat: follow service added

---
+++ src/main/java/goojeans/harulog/user/service/FollowServiceImpl.java
@@ -0,0 +1,141 @@
+package goojeans.harulog.user.service;
+
+import goojeans.harulog.domain.BusinessException;
+import goojeans.harulog.domain.ResponseCode;
+import goojeans.harulog.domain.dto.Response;
+import goojeans.harulog.user.domain.dto.JwtUserDetail;
+import goojeans.harulog.user.domain.dto.request.FollowRequest;
+import goojeans.harulog.user.domain.dto.response.FollowInfo;
+import goojeans.harulog.user.domain.entity.Follow;
+import goojeans.harulog.user.domain.entity.FollowId;
+import goojeans.harulog.user.domain.entity.Users;
+import goojeans.harulog.user.repository.FollowRepository;
+import goojeans.harulog.user.repository.UserRepository;
+import goojeans.harulog.user.util.SecurityUtils;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Slf4j
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class FollowServiceImpl implements FollowService {
+
+    private final UserRepository userRepository;
+    private final FollowRepository followRepository;
+    private final SecurityUtils securityUtils;
+
+    @Override
+    public Response<List<FollowInfo>> getFollowerList(String nickname) {
+
+        Users user = userRepository.findByNickname(nickname).stream()
+                .findAny()
+                .orElseThrow(() -> new BusinessException(ResponseCode.USER_NOT_FOUND));
+
+        List<FollowInfo> list = user.getFollowers().stream()
+                .map(follow -> FollowInfo.entityToResponse(follow.getFollowing()))
+                .toList();
+
+        return Response.ok(list);
+
+    }
+
+    @Override
+    public Response<List<FollowInfo>> getMyFollowerList() {
+
+        JwtUserDetail currentUserInfo = securityUtils.getCurrentUserInfo();
+
+        List<FollowInfo> list = followRepository.findFollowerByUserId(currentUserInfo.getId()).stream()
+                .map(follow -> FollowInfo.entityToResponse(follow.getFollower()))
+                .toList();
+
+        return Response.ok(list);
+
+    }
+
+    @Override
+    public Response<List<FollowInfo>> getFollowingList(String nickname) {
+
+        Users user = userRepository.findByNickname(nickname).stream()
+                .findAny()
+                .orElseThrow(() -> new BusinessException(ResponseCode.USER_NOT_FOUND));
+
+        List<FollowInfo> list = user.getFollowings().stream()
+                .map(follow -> FollowInfo.entityToResponse(follow.getFollower()))
+                .toList();
+
+        return Response.ok(list);
+
+    }
+
+    @Override
+    public Response<List<FollowInfo>> getMyFollowingList() {
+
+        JwtUserDetail currentUserInfo = securityUtils.getCurrentUserInfo();
+
+        List<FollowInfo> list = followRepository.findFollowingByUserId(currentUserInfo.getId()).stream()
+                .map(follow -> FollowInfo.entityToResponse(follow.getFollowing()))
+                .toList();
+
+        return Response.ok(list);
+
+    }
+
+    // 성능 개선 필요 -> join 이 2번 있는 쿼리가 2번 나감 : 총 4번의 join
+    @Override
+    public Response<Void> follow(FollowRequest request) {
+
+        String nickname = securityUtils.getCurrentUserInfo().getNickname();
+
+        Users follower = userRepository.findByNickname(nickname).stream()
+                .findAny()
+                .orElseThrow(() -> new BusinessException(ResponseCode.USER_NOT_FOUND));
+
+        Users following = userRepository.findByNickname(request.getTo()).stream()
+                .findAny()
+                .orElseThrow(() -> new BusinessException(ResponseCode.USER_NOT_FOUND));
+
+        Follow follow = Follow.builder()
+                .follower(follower)
+                .following(following)
+                .build();
+
+        follower.addFollowing(follow);
+        following.addFollower(follow);
+
+        return Response.ok();
+
+    }
+
+    @Override
+    public Response<Void> followingDelete(String nickname) {
+        JwtUserDetail currentUserInfo = securityUtils.getCurrentUserInfo();
+
+        Users following = userRepository.findUsersByNickname(nickname).stream()
+                .findAny()
+                .orElseThrow(() -> new BusinessException(ResponseCode.USER_NOT_FOUND));
+
+        followRepository.deleteFollow(new FollowId(currentUserInfo.getId(), following.getId()));
+
+        return Response.ok();
+
+    }
+
+    @Override
+    public Response<Void> followerDelete(String nickname) {
+        JwtUserDetail currentUserInfo = securityUtils.getCurrentUserInfo();
+
+        Users follower = userRepository.findUsersByNickname(nickname).stream()
+                .findAny()
+                .orElseThrow(() -> new BusinessException(ResponseCode.USER_NOT_FOUND));
+
+        followRepository.deleteFollow(new FollowId(follower.getId(), currentUserInfo.getId()));
+
+        return Response.ok();
+
+    }
+}
