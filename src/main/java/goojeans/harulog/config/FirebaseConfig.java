d1afc9486d85f8f557c2d32b63ddb7c9079444e6
Keon <lune0410k@naver.com>
2024-02-14 16:56:11 +0900

    fix: base64 encoding use for env injection Escape character cannot convert so use Base64 Encoding.

--- src/main/java/goojeans/harulog/config/FirebaseConfig.java
+++ src/main/java/goojeans/harulog/config/FirebaseConfig.java
@@ -3,24 +3,29 @@ package goojeans.harulog.config;
 import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
+import java.util.Base64;
 
+@Slf4j
 @Configuration
 public class FirebaseConfig {
 
     @Bean
-    public FirebaseApp firebaseApp(@Value("${firebase.key}")String auth) throws IOException {
+    public FirebaseApp firebaseApp(@Value("${firebase.key}")String envString) throws IOException {
+
+        String auth = new String(Base64.getDecoder().decode(envString));
 
         ByteArrayInputStream byteArrayInputStream = new ByteArrayInputStream(auth.getBytes());
 
         FirebaseOptions options = FirebaseOptions.builder()
                 .setCredentials(GoogleCredentials.fromStream(byteArrayInputStream))
-                .setStorageBucket("haru-log-test.appspot.com")
+                .setStorageBucket("harulogtest.appspot.com")
                 .build();
 
         FirebaseApp app = FirebaseApp.initializeApp(options);
