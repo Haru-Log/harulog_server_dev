29c68f3c5b062c3f09d4dd5004e5c23a1a3d9445
HJunng <guswjd1gh@gmail.com>
2024-02-16 16:06:11 +0900

    feat: 메세지 보낼 때 messageType도 입력받도록 변경

--- src/main/java/goojeans/harulog/chat/domain/dto/request/MessageRequest.java
+++ src/main/java/goojeans/harulog/chat/domain/dto/request/MessageRequest.java
@@ -1,6 +1,7 @@
 package goojeans.harulog.chat.domain.dto.request;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import goojeans.harulog.chat.util.MessageType;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -14,6 +15,9 @@ import lombok.NoArgsConstructor;
 public class MessageRequest {
     @JsonProperty("sender")
     private String sender; // 유저 닉네임
+
+    @JsonProperty("messageType")
+    private MessageType messageType; // 메세지 타입 (IMAGE, TALK)
 
     @JsonProperty("content")
     private String content; // 메세지 내용
