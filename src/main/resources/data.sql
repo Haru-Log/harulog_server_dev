adf0900dcae800d49b8406c9fa67bd0f3f6aab7d
Keon <lune0410k@naver.com>
2024-02-16 16:21:07 +0900

    refactor: admin account edited

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -4,4 +4,4 @@ INSERT INTO category (category_name, created_at, updated_at, active_status) VALU
 INSERT INTO category (category_name, created_at, updated_at, active_status) VALUES ('공부', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, 'ACTIVE');
 
 INSERT INTO users (created_at, updated_at, user_id, email, nickname, password, user_name, active_status, social_type, user_role)
- VALUES('2024-01-01', '2024-01-01', 1, 'local', 'local', '{bcrypt}$2a$10$EKWzqU3GOsgCjOYbKZWYhe4s1.Unz8t6ooyQO6WCP3ksdsn.KLMXe', 'local', 'ACTIVE', 'HARU', 'ADMIN');
+ VALUES('2024-01-01', '2024-01-01', 1, 'super@admin.admin', 'admin', '{bcrypt}$2a$10$jvh.as2kE7F846rJJUug3OLNG7B48yLSFWrMpG1i4MbHsKVCUS5Bm', 'admin', 'ACTIVE', 'HARU', 'ADMIN');
